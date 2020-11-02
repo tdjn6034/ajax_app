675574dc5079a6f728d70e952c5993c4f4bd0607
tdjn6034 <wfcpbumc_g_sc_eb@yahoo.co.jp>
2020-11-03 05:58:23 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,12 @@
 class PostsController < ApplicationController
+
   def index
-    @post = Post.find(1)
+    @posts = Post.all.order(id: "DESC")
   end
+
+  def create
+    Post.create(content: params[:content])
+    redirect_to action: :index
+  end
+
 end
